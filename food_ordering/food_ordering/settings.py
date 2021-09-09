21ac98ff5e646c496a130aacdf218026950bb539
Bilab Thapa <thapabilab9@gmail.com>
2021-09-09 10:41:35 +0545

    project setup

--- food_ordering/food_ordering/settings.py
+++ food_ordering/food_ordering/settings.py
@@ -81,8 +81,14 @@ WSGI_APPLICATION = 'food_ordering.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'foods',
+        'USER': 'root',
+        'PASSWORD': 'Bilab@3518',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': '',
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLE'"
     }
 }
 
